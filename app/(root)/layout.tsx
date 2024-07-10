409bc71eee4ef51ab22f2478e54644104e34f4cd
Pranit Deshpande <71553492+pranitdeshpande98@users.noreply.github.com>
2024-07-10 10:22:55 -0400

    inital header changes

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,13 @@
+import SideBar from "@/components/SideBar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <main>
-        SIDEBAR 
+    <main className="flex h-screen w-full font-inter">
+        <SideBar /> 
         {children}
     </main>
   );
